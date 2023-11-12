dc049eeda942b3d8367822dc0a7079adcd4062ef
Jamal Majid <45414121+jamalabd@users.noreply.github.com>
2023-11-12 20:52:36 +0000

    basic server created

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require('express');
+
+const app = express();
+
+
+app.get('/',((req, res)=>{
+    res.send('Task Manager app.')
+}))
+
+
+
+const port = 3000;
+
+app.listen(port, console.log(` server is listening on port ${port}`));
